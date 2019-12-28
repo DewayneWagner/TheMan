43bc756d120723ba81703153f6dc2328e5a923c4
Dewayne Wagner <dewayne@rockyspring.ca>
2019-12-27 17:48:36 -0700

    SQ's can be found in db by row / column, but not by key.

--- TheManXS/TheManXS/ViewModel/MapBoardVM/MapConstruct/Map.cs
+++ TheManXS/TheManXS/ViewModel/MapBoardVM/MapConstruct/Map.cs
@@ -20,6 +20,9 @@ namespace TheManXS.ViewModel.MapBoardVM.MapConstruct
         MapVM _mapVM;
 
         private System.Random rnd = new System.Random();
+        private SQ sq;
+        private SKRect rect;
+        private TileConstructCalc t;
 
         SKPaint miniPaint = new SKPaint()
         {
@@ -53,83 +56,115 @@ namespace TheManXS.ViewModel.MapBoardVM.MapConstruct
                     {
                         for (int row = 0; row < QC.RowQ; row++)
                         {
-                            SQ sq = getSQ(row, col);
+                            sq = getSQ(row, col);
+                            rect = getSKRect();
+                            setTileFormat();
 
-                            TileConstructCalc t = new TileConstructCalc(_mapVM, row, col);
-                            int q = sq.TerrainType == TerrainTypeE.Mountain ? rnd.Next(25,50) : rnd.Next(5, 10);
-                            SKRect rect = new SKRect(col * QC.SqSize, row * QC.SqSize, (col + 1) * QC.SqSize, (row + 1) * QC.SqSize);
-
-                            switch (t.GetFormat(sq.TerrainType))
-                            {
-                                case sqFormats.LinearGradient:
-                                    Tuple<SKPoint, SKPoint> points = t.GetGradientPoints();
-                                    standardTilePaint.Shader = SKShader.CreateLinearGradient(
-                                        points.Item1,
-                                        points.Item2,
-                                        t.GetGradientColors(q,sq.TerrainType),
-                                        t.GetColorPosition(q),
-                                        SKShaderTileMode.Clamp);
-                                    break;
-
-                                case sqFormats.SolidColor:
-                                    standardTilePaint.Color = TerrainColors.GetRandomColor(sq.TerrainType);
-                                    break;
-
-                                case sqFormats.SweepGradient:
-                                    standardTilePaint.Shader = SKShader.CreateSweepGradient(t.GetCenterPoint(),
-                                        t.GetGradientColors(q,sq.TerrainType), t.GetColorPosition(q));
-                                    break;
-
-                                case sqFormats.VerticalSplit:
-                                    float x = (float)(rnd.Next(col * QC.SqSize, (col + 1) * QC.SqSize));
-                                    standardTilePaint.Shader = SKShader.CreateLinearGradient(
-                                        new SKPoint(x, (row * QC.SqSize)),
-                                        new SKPoint(x, ((row + 1) * QC.SqSize)),
-                                        t.GetGradientColors(q,sq.TerrainType),
-                                        t.GetColorPosition(q),
-                                        SKShaderTileMode.Clamp);
-                                    break;
-
-                                case sqFormats.HorizontalSplit:
-                                    float y = (float)(rnd.Next(row * QC.SqSize, ((row + 1) * QC.SqSize)));
-                                    standardTilePaint.Shader = SKShader.CreateLinearGradient(
-                                        new SKPoint(col * QC.SqSize, y),
-                                        new SKPoint((col + 1) * QC.SqSize, y),
-                                        t.GetGradientColors(q,sq.TerrainType),
-                                        t.GetColorPosition(q),
-                                        SKShaderTileMode.Clamp);
-                                    break;
-
-                                case sqFormats.Pixelly:
-                                    int sqsPerSide = 25;
-                                    float miniSqSize = QC.SqSize / sqsPerSide;
-
-                                    for (int miniRow = 0; miniRow < sqsPerSide; miniRow++)
-                                    {
-                                        for (int miniCol = 0; miniCol < sqsPerSide; miniCol++)
-                                        {
-                                            miniPaint.Color = TerrainColors.GetRandomColor(TerrainTypeE.Forest);
-
-                                            SKRect miniRect = new SKRect(
-                                                ((miniCol * miniSqSize) + (QC.SqSize * col)),
-                                                ((miniRow * miniSqSize) + (QC.SqSize * row)),
-                                                (((miniCol + 1) * miniSqSize) + (QC.SqSize * col)),
-                                                (((miniRow + 1) * miniSqSize) + (QC.SqSize * row)));                                                
-                                            gameboard.DrawRect(miniRect, miniPaint);
-                                        }
-                                    }
-                                    break;
-                                default:
-                                    break;
-                            }
                             if (sq.TerrainType == TerrainTypeE.Grassland) { standardTilePaint.BlendMode = SKBlendMode.Hue; }
                             else if (sq.TerrainType == TerrainTypeE.Mountain) { standardTilePaint.BlendMode = SKBlendMode.Screen; }
-                            if (sq.TerrainType != TerrainTypeE.Forest) { gameboard.DrawRect(rect, standardTilePaint); }                            
+                            
+                            if (sq.TerrainType != TerrainTypeE.Forest) { gameboard.DrawRect(rect, standardTilePaint); }
                         }
                     }
                     gameboard.Save();
+
+                    void setTileFormat()
+                    {
+                        t = new TileConstructCalc(_mapVM, sq.Row, sq.Col);
+                        switch (t.GetFormat(sq.TerrainType))
+                        {
+                            case sqFormats.LinearGradient:
+                                setLinearGradient();
+                                break;
+                            case sqFormats.SolidColor:
+                                standardTilePaint.Color = TerrainColors.GetRandomColor(sq.TerrainType);
+                                break;
+                            case sqFormats.SweepGradient:
+                                setSweepGradient();
+                                break;
+                            case sqFormats.VerticalSplit:
+                                setVerticalSplit();
+                                break;
+                            case sqFormats.HorizontalSplit:
+                                setHorizontalSplit();
+                                break;
+                            case sqFormats.Pixelly:
+                                setPixelly(gameboard);
+                                break;
+                        }
+                    }
                 }
+
                 SQ getSQ(int row, int col) => db.SQ.Where(s => s.Row == row).Where(s => s.Col == col).FirstOrDefault();
+                SKRect getSKRect() => new SKRect(sq.Col * QC.SqSize, sq.Row * QC.SqSize, (sq.Col + 1) * QC.SqSize, (sq.Row + 1) * QC.SqSize);
+                int getQ(TerrainTypeE tt) => tt == TerrainTypeE.Mountain ? rnd.Next(25, 50) : rnd.Next(5, 10);
+                
+                void setLinearGradient()
+                {
+                    int q = getQ(sq.TerrainType);
+                    Tuple<SKPoint, SKPoint> points = t.GetGradientPoints();
+                    standardTilePaint.Shader = SKShader.CreateLinearGradient(
+                        points.Item1,
+                        points.Item2,
+                        t.GetGradientColors(q, sq.TerrainType),
+                        t.GetColorPosition(q),
+                        SKShaderTileMode.Clamp);
+                }
+
+                void setSweepGradient()
+                {
+                    int q = getQ(sq.TerrainType);
+                    standardTilePaint.Shader = SKShader.CreateSweepGradient(t.GetCenterPoint(),
+                        t.GetGradientColors(q, sq.TerrainType), t.GetColorPosition(q));
+                }
+
+                void setVerticalSplit()
+                {
+                    int q = getQ(sq.TerrainType);
+
+                    float x = (float)(rnd.Next(sq.Col * QC.SqSize, (sq.Col + 1) * QC.SqSize));
+                    standardTilePaint.Shader = SKShader.CreateLinearGradient(
+                        new SKPoint(x, (sq.Row * QC.SqSize)),
+                        new SKPoint(x, ((sq.Row + 1) * QC.SqSize)),
+                        t.GetGradientColors(q,sq.TerrainType),
+                        t.GetColorPosition(q),
+                        SKShaderTileMode.Clamp);
+                }
+
+                void setHorizontalSplit()
+                {
+                    int q = getQ(sq.TerrainType);
+                    float y = (float)(rnd.Next(sq.Row * QC.SqSize, ((sq.Row + 1) * QC.SqSize)));
+                    standardTilePaint.Shader = SKShader.CreateLinearGradient(
+                        new SKPoint(sq.Col * QC.SqSize, y),
+                        new SKPoint((sq.Col + 1) * QC.SqSize, y),
+                        t.GetGradientColors(q, sq.TerrainType),
+                        t.GetColorPosition(q),
+                        SKShaderTileMode.Clamp);
+                }
+
+                void setPixelly(SKCanvas gameboard)
+                {
+                    int sqsPerSide = 25;
+                    float miniSqSize = QC.SqSize / sqsPerSide;
+                    int colPosition = QC.SqSize * sq.Col;
+                    int rowPosition = QC.SqSize * sq.Row;
+
+                    for (int miniRow = 0; miniRow < sqsPerSide; miniRow++)
+                    {
+                        for (int miniCol = 0; miniCol < sqsPerSide; miniCol++)
+                        {
+                            miniPaint.Color = TerrainColors.GetRandomColor(TerrainTypeE.Forest);
+
+                            SKRect miniRect = new SKRect(
+                                ((miniCol * miniSqSize) + colPosition),
+                                ((miniRow * miniSqSize) + rowPosition),
+                                (((miniCol + 1) * miniSqSize) + colPosition),
+                                (((miniRow + 1) * miniSqSize) + rowPosition));
+                            gameboard.DrawRect(miniRect, miniPaint);
+                        }
+                    }
+                }
             }
         }
     }
